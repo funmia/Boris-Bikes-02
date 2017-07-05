eea3683617417867e4e2fe01d8f300170e59d2ab
nandhini <nandhini31@gmail.com>
2017-07-05 14:49:51 +0100

    Challenge 10

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,6 +1,6 @@
+
 class Bike
   def working?
-
-  end  
-
+    true
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,16 @@
 require "docking_station"
+require "bike"
 
 describe DockingStation do
   describe '#release_bike' do
     it "responds to release_bike method " do
       expect(subject).to respond_to(:release_bike)
+    end
+    it "expects release_method to return a Bike" do
+      expect(subject.release_bike).to be_instance_of(Bike)
+    end
+    it "expects the Bike #working?" do
+      expect(subject.release_bike.working?).to be true
     end
   end
 end
